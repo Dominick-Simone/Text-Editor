c15fa5980f591d4adc2c4d6454ad307f52374514
Dominick Simone <domsimone123@gmail.com>
2021-10-16 18:33:15 -0400

    finishing app already installed event listener

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -27,5 +27,5 @@ butInstall.addEventListener('click', async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-
+    window.deferredPrompt = null;
 });
